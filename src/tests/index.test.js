81e5290e3038677b3efdab913c766bd97ba85ac0
Lawrence Yoon <lyoon@truechoice.io>
2022-06-14 09:34:43 -0700

    feat: fail jest test, check if second job runs

--- src/tests/index.test.js
+++ src/tests/index.test.js
@@ -1,5 +1,5 @@
 const sum = require('../');
 
 describe('Sum function:', () => {
-  test('Arguments 1 and 3 should output 4', () => expect(sum(1, 3)).toBe(4));
+  test('Arguments 1 and 3 should output 4', () => expect(sum(1, 3)).toBe(5));
 });
